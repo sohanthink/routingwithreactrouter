6b1620f207fc046d487e965b84730288fa4d3fef
Muhammad Sohan Mollah <sohansourab@gmail.com>
2023-12-06 03:16:38 +0600

    usestate hook/increase decrease

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+    let [count, setCount] = useState(10)
+    let handleIncrement = () => {
+        count++
+        setCount(count)
+    }
+    let handleDecrement = () => {
+        count--
+        setCount(count)
+    }
     return (
-        <h1>Home page</h1>
+        <>
+            <h1>Home page</h1>
+            <button onClick={handleIncrement}>Increment</button>
+            <h1>{count}</h1>
+            <button onClick={handleDecrement}>decrement</button>
+        </>
     )
 }
 
