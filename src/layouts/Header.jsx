6a03ea8840c71eaddcdf2fdd2c905eb6355483f6
Muhammad Sohan Mollah <sohansourab@gmail.com>
2023-12-05 23:55:52 +0600

    routing added for page change

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
         <div className="navbar">
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Service</li>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/service">Service</NavLink>
+                </li>
             </ul>
         </div>
     )
