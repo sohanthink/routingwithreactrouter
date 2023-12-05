4ac04e750f23817e4fc4125250620c9a41cabb9e
Muhammad Sohan Mollah <sohansourab@gmail.com>
2023-12-05 23:38:30 +0600

    comon layout added/header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,13 @@ import {
 import Home from "./pages/Home"
 import About from './pages/About';
 import Service from './pages/Service';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route>
+      <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
