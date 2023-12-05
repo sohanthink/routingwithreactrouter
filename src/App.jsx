96dbd49aaf8561e4565635637efab996862e105a
Muhammad Sohan Mollah <sohansourab@gmail.com>
2023-12-06 02:43:23 +0600

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,20 @@ import Home from "./pages/Home"
 import About from './pages/About';
 import Service from './pages/Service';
 import MainLayout from './layouts/MainLayout';
+import Error from './layouts/Error';
 
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/service" element={<Service />} />
-      </Route>
+      <>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </>
     )
   );
 
---
+++ src/layouts/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./error.css"
+import { Link } from 'react-router-dom'
+const Error = () => {
+    return (
+        <>
+            <h6>error</h6>
+            <Link to='/'>Back to Home</Link>
+        </>
+    )
+}
+
+export default Error
