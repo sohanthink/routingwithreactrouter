0088d1c42680d511a0d985b77e132c3b88393785
Muhammad Sohan Mollah <sohansourab@gmail.com>
2023-12-05 22:59:49 +0600

    react roiuting setting up completed

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home"
+import About from './pages/About';
+import Service from './pages/Service';
+
+function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+      </Route>
+    )
+  );
+
+  return (
+    <RouterProvider
+      router={router}
+    />
+  )
+}
+
+export default App
---
+++ src/pages/Service.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Service = () => {
+    return (
+        <h1>This is service</h1>
+    )
+}
+
+export default Service
